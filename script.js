a2bcb35fe745be343351be1ccb043364c7d5215a
Roadtrip Horror <aaronfrankenfield@gmail.com>
2020-12-12 12:58:27 -0500

    first commit for functions repo

---
+++ script.js
@@ -0,0 +1,39 @@
+// // defining a function
+// function sayHello() {
+//   console.log("hello world");
+// }
+
+// // call a function/execute a function
+// sayHello();
+// sayHello();
+
+// // I Love JavaScript
+// // Define a function that logs the string "I love JavaScript!" to the console.
+// // Execute, or call, the function.
+
+// function printPhrase() {
+//   console.log("I love JavaScript");
+// }
+
+// printPhrase();
+
+// function sayHello(friendName, greeting) {
+//   console.log(`${greeting}, ${friendName}`);
+// }
+
+// sayHello("Joseph", "Heya");
+// sayHello("Thomas", "Howdy");
+// sayHello("Aaron", "Hey");
+// sayHello("Tommy", "Hi");
+
+// Write a function that accepts two parameters of name and destination.
+function travelPlanner(name, destination) {
+  console.log(`${name} wants to go to ${destination}.`);
+}
+
+// The function should log a sentence to the console about where that person wants to go.
+// Call the function three different times with different parameters. Example: if you pass in "Jessica" and "Mount Fuji" as parameters, you should see the sentence "Jessica would love to visit Mount Fuji." logged to the console.
+travelPlanner("Aaron", "New Orleans");
+travelPlanner("Mary", "Italy");
+travelPlanner("Ringo", "Japan");
+
